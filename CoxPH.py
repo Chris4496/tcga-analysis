9f7f1ba9f25c4beaf5de8ae929c8282d3a688f26
Chris4496 <62294842+Chris4496@users.noreply.github.com>
2024-10-24 21:52:02 +0800

    Added cross validation for hyperparameter tuning

--- CoxPH.py
+++ CoxPH.py
@@ -2,22 +2,24 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from lifelines import CoxPHFitter, KaplanMeierFitter
+from lifelines.utils import k_fold_cross_validation
+from pprint import pprint
 
 from pickle import load, dump
 
 top = 500
     
-def CoxRegression(df, gene_names, penalty=0.1, l1_ratio=0.1):
-    # create new df that have the nessecary columns
-    temp = df[['time_days', 'patient_dead', *gene_names]]
+# def CoxRegression(df, gene_names, penalty=0.1, l1_ratio=0.1):
+#     # create new df that have the nessecary columns
+#     temp = df[['time_days', 'patient_dead', *gene_names]]
 
-    # fit cox regression
-    model = CoxPHFitter(penalizer=penalty, l1_ratio=l1_ratio)
-    model.fit(temp, duration_col="time_days", event_col="patient_dead")
+#     # fit cox regression
+#     model = CoxPHFitter(penalizer=penalty, l1_ratio=l1_ratio)
+#     model.fit(temp, duration_col="time_days", event_col="patient_dead")
 
-    # model.print_summary()
+#     # model.print_summary()
 
-    return model
+#     return model
 
 
 def main():
@@ -32,26 +34,34 @@ def main():
 
     # Hyperparameters tuning
     top_x = [10, 20, 30, 50, 100, 200, 300, 400, 500, 1000]
-    penalties = [10, 5, 1, 0.5, 0.1, 0.05, 0.01, 0.001]
-    l1_ratios = [1.0, 0.5, 0.1, 0.05, 0.01]
+    penalties = [10, 5, 1, 0.5, 0.1]
+    l1_ratios = [1.0, 0.8, 0.5, 0.3, 0.1, 0]
 
-    for tx in top_x:
-        for penalty in penalties:
-            for l1_ratio in l1_ratios:
-                print(f"Fitting CoxPH with penalty={penalty}, l1_ratio={l1_ratio}, top={tx}")
-                try:
-                    model = CoxRegression(merged, [x.index[0] for x in univariate_results_sorted[:tx]], penalty=penalty, l1_ratio=l1_ratio)
-                except:
-                    print(f"Failed to fit CoxPH with penalty={penalty}, l1_ratio={l1_ratio}, top={tx}")
-                    continue
-                
-                # save results
-                with open(f'models/cox_ph_top{tx}_penalty{penalty}_l1_ratio{l1_ratio}.pkl', 'wb') as f:
-                    dump(model, f)
+    # top 300 genes
+    top = [x.index[0] for x in univariate_results_sorted[:30]]
 
+    # get top 300 genes
+    temp = merged[['time_days', 'patient_dead', *top]]
 
+    cph_list = list()
 
+    for penatly in penalties:
+        for l1 in l1_ratios:
+            cph = CoxPHFitter(penalizer=penatly, l1_ratio=l1)
+            cph_list.append(cph)
 
+    scores = k_fold_cross_validation(cph_list, temp, duration_col='time_days', event_col='patient_dead', scoring_method='concordance_index')
+
+    # take the mean
+    scores = [sum(x)/len(x) for x in scores]
+
+    scores = np.array(scores).reshape(len(penalties), len(l1_ratios))
+
+    print(scores)
+
+    # save results
+    with open('cache/c_index_top30_penalty_vs_l1_ratio.pkl', 'wb') as f:
+        dump(scores, f)
 
 
 if __name__ == "__main__":
--- ParameterAnalysis.py
+++ ParameterAnalysis.py
@@ -5,42 +5,32 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    # Hyperparameters
-    top_x = [10, 20, 30, 50, 100, 200, 300, 400, 500, 1000]
-    penalties = [10, 5, 1, 0.5, 0.1, 0.05, 0.01, 0.001, 0.0001, 0.00001]
-    l1_ratios = [1.0, 0.5, 0.1, 0.05, 0.01, 0.001, 0.0001, 0.00001]
+    # read data
+    with open('cache/c_index_top30_penalty_vs_l1_ratio.pkl', 'rb') as f:
+        c_index = load(f)
 
-    for tx in top_x:
-        c_indices = list()
-        for penalty in penalties:
-            penrow = list()
-            for l1_ratio in l1_ratios:
-                try:
-                    with open(f'models/cox_ph_top{tx}_penalty{penalty}_l1_ratio{l1_ratio}.pkl', 'rb') as f:
-                        model = load(f)
-                    c_index = model.concordance_index_
-                except:
-                    print(f"Failed to load model for penalty={penalty}, l1_ratio={l1_ratio}, top={tx}")
-                    c_index = np.nan
-                penrow.append(c_index)
-            c_indices.append(penrow)
+    print(c_index)
 
-        # plot heatmap
-        plt.figure(figsize=(10, 6))  # Set the figure size
-        plt.imshow(c_indices, cmap='viridis', interpolation='nearest')
-        plt.colorbar()
-        plt.xticks(range(len(l1_ratios)), [f'l1_ratio={l1_ratio}' for l1_ratio in l1_ratios], rotation=90)
-        plt.yticks(range(len(penalties)), [f'penalty={penalty}' for penalty in penalties])
-        plt.xlabel('L1 ratio')
-        plt.ylabel('Penalty')
-        plt.title(f'CoxPH Concordance Index Top {tx}')
-        plt.tight_layout()  # Adjust layout to prevent label cutoffs
-        plt.show()
+    penalties = [10, 5, 1, 0.5, 0.1]
+    l1_ratios = [1.0, 0.8, 0.5, 0.3, 0.1, 0]
 
-        # reset figure
-        plt.clf()
-        plt.close()
+    # Create the heatmap
+    plt.imshow(c_index, cmap='viridis', interpolation='nearest')
 
+    # Add colorbar
+    plt.colorbar(label='Value')
+
+    # Set the ticks and labels for the x-axis and y-axis
+    plt.xticks(ticks=np.arange(len(l1_ratios)), labels=l1_ratios)
+    plt.yticks(ticks=np.arange(len(penalties)), labels=penalties)
+
+    # Add labels and title
+    plt.xlabel('L1 Ratios')
+    plt.ylabel('Penalties')
+    plt.title('Heatmap with Penalties and L1 Ratios')
+
+    # Save image
+    plt.savefig('output/log_likihood_heatmap_penalty_vs_l1_ratio.png', dpi=300)
 
 if __name__ == "__main__":
     main()
